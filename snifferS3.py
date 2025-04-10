f4794ef1ebb725038845b757aad7f83b0a666063
Donchka <mhd993065259@gmail.com>
2025-04-10 15:27:56 -0400

    Add files via upload

--- snifferS3.py
+++ snifferS3.py
@@ -21,7 +21,7 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
-from ryu.lib.packet import ipv4
+from ryu.lib.packet import ipv4,tcp,udp
 # import json
 # import boto3
 # from botocore.exceptions import NoCredentialsError
@@ -42,7 +42,7 @@ class SimpleSwitch13(app_manager.RyuApp):
             host="sdn-db.ctmg0o4y2obu.us-east-2.rds.amazonaws.com",
             user="admin",
             password="Humbersdn1",
-            database="sdn-db"
+            database="packet_logs"
         )
         self.db_cursor = self.db_connection.cursor()
 
@@ -62,21 +62,26 @@ class SimpleSwitch13(app_manager.RyuApp):
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
-        self.add_flow(datapath, 0, match, actions)
+        self.add_flow(datapath, 0, match, actions,idle_timeout=0,hard_timeout=0)
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None, idle_timeout=5, hard_timeout=5):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
                                              actions)]
+        
         if buffer_id:
             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
                                     priority=priority, match=match,
-                                    instructions=inst)
+                                    instructions=inst,
+                                    idle_timeout=idle_timeout,
+                                    hard_timeout=hard_timeout)
         else:
             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
+                                    match=match, instructions=inst,
+                                    idle_timeout=idle_timeout,
+                                    hard_timeout=hard_timeout)
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
